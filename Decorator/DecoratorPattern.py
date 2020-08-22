1ea5e765c2591a3118bbfd08656f3fc84a9a40e8
TimChow <744475502@qq.com>
2020-08-23 01:08:23 +0800

    Update DecoratorPattern.py

--- Decorator/DecoratorPattern.py
+++ Decorator/DecoratorPattern.py
@@ -1,62 +1,55 @@
-#coding: utf8
+# coding: utf8
 
-import time
+import abc
 
-"""
-python在语言层面就支持装饰器模式，
-下面用一个绕口的方式，说明python中的装饰器：
-    装饰器是一个可调用对象
-    装饰器需要返回一个可调用对象
-    装饰器用来装饰可调用对象
-python中的可调用对象包括：
-    函数
-    方法
-    实现了__call__方法的对象
-    类
-装饰器分为两类：
-    不带参数的装饰器
-    带参数的装饰器
 
-@decorator
-def func():
-    pass
-等同于 func = decorator(func)
+class Component(object):
+    __metaclass__ = abc.ABCMeta
 
-@decorator2
-@decorator1
-def func():
-    pass
-等同于 func = decorator2(decorator1(func))
+    @abc.abstractmethod
+    def process(self):
+        pass
 
-@decorator(arg)
-def func():
-    pass
-等同于 func = decorator(arg)(func)
-"""  
 
-class LogDecorator:
-    def __init__(self, wrapped_callable_object):
-        self._callable = wrapped_callable_object
+class ConcreteComponent(Component):
+    def process(self):
+        print("process in ConcreteComponent")
 
-    def __call__(self, *a, **kw):
-        print("begin to invoked %s with %s, %s" % (self._callable, a, kw))
-        result = self._callable(*a, **kw)
-        print("invoked succesfully")
-        return result
 
-def execution_time_decorator(func):
-    def _inner(*a, **kw):
-        start_time = time.time()
-        result = func(*a, **kw)
-        print("%s executed in %s s" % (func, time.time()-start_time))
-        return result
-    return _inner
+class Decorator(Component):
+    def __init__(self, component):
+        self._component = component
+
+    def process(self):
+        self.before()
+        self._component.process()
+        self.after()
+
+    @abc.abstractmethod
+    def before(self):
+        pass
+
+    @abc.abstractmethod
+    def after(self):
+        pass
+
+
+class LogDecorator(Decorator):
+    def before(self):
+        print("before calling process")
+
+    def after(self):
+        print("after calling process")
+
 
 if __name__ == "__main__":
-    @LogDecorator
-    @execution_time_decorator
-    def func1():
-        time.sleep(1)
+    import unittest
 
-    func1()
 
+    class DecoratorTest(unittest.TestCase):
+        def testDecorator(self):
+            component = ConcreteComponent()
+            decorator = LogDecorator(component)
+            decorator.process()
+
+    unittest.main()
