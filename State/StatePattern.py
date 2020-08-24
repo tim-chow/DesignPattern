f7861378c28fea6bc49c5e2d46ea163363e342ec
TimChow <744475502@qq.com>
2020-08-24 17:46:52 +0800

    Update StatePattern.py

--- State/StatePattern.py
+++ State/StatePattern.py
@@ -1,86 +1,105 @@
-class Context:
+# coding: utf8
+
+
+class Context(object):
+    """环境角色"""
     def __init__(self):
         self._open = OpenState()
-        self._close = CloseState()
-        self._cool = CoolState()
-        self._heat = HeatState()
-        self._current_state = self._close;
+        self._closed = ClosedState()
+        self._cooling = CoolingState()
+        self._heating = HeatingState()
+        self._current_state = self._closed
 
-    def to_open(self):
+    def open(self):
         if self._current_state.open():
             self._current_state = self._open
+        else:
+            print("can't change to open from %s" % self._current_state.get_name())
 
-    def to_close(self):
+    def close(self):
         if self._current_state.close():
-            self._current_state = self._close
+            self._current_state = self._closed
+        else:
+            print("can't change to closed from %s" % self._current_state.get_name())
 
-    def to_cool(self):
+    def cool(self):
         if self._current_state.cool():
-            self._current_state = self._cool
+            self._current_state = self._cooling
+        else:
+            print("can't change to cooling from %s" % self._current_state.get_name())
 
-    def to_heat(self):
+    def heat(self):
         if self._current_state.heat():
-            self._current_state = self._heat
+            self._current_state = self._heating
+        else:
+            print("can't change to heating from %s" % self._current_state.get_name())
 
-class AirContiditoningState:
+
+class AirConditionState(object):
+    """抽象状态角色"""
     def open(self):
-        print("illegal state, unable to open")
         return False
 
     def close(self):
-        print("illegal state, unable to close")
         return False
 
     def cool(self):
-        print("illegal state, unable to cool")
         return False
 
     def heat(self):
-        print("illegal state, unable to heat")
         return False
 
-class OpenState(AirContiditoningState):
+    def get_name(self):
+        return self.__class__.__name__[:-1 * len("State")].lower()
+
+
+class OpenState(AirConditionState):
+    """开机状态"""
     def close(self):
-        print("change to state: `close`")
+        print("closed")
         return True
 
     def cool(self):
-        print("change to state: `cool`")
+        print("cooling")
         return True
 
     def heat(self):
-        print("change to state: `heat`")
+        print("heating")
         return True
 
-class CloseState(AirContiditoningState):
+
+class ClosedState(AirConditionState):
+    """关机状态"""
     def open(self):
-        print("change to state: `open`")
+        print("open")
         return True
 
-class CoolState(AirContiditoningState):
+
+class CoolingState(AirConditionState):
+    """制冷状态"""
     def close(self):
-        print("change to state: `close`")
+        print("closed")
         return True
 
     def heat(self):
-        print("change to state: `heat`")
+        print("heating")
         return True
 
-class HeatState(AirContiditoningState):
+
+class HeatingState(AirConditionState):
+    """制冷状态"""
     def close(self):
-        print("change to state: `close`")
+        print("closed")
         return True
 
     def cool(self):
-        print("change to state: `cool`")
+        print("cooling")
         return True
+
 
 if __name__ == "__main__":
     context = Context()
-    context.to_heat();
-    context.to_open();
-    context.to_heat();
-    context.to_cool();
-    context.to_close();
-    context.to_close();
-
+    context.open()
+    context.heat()
+    context.close()
+    context.cool()
