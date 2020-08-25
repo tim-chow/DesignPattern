fbe9e4b42408e5ecb037df400d618edb6c3cb553
TimChow <744475502@qq.com>
2020-08-25 11:47:24 +0800

    Update ProtoType.py

--- ProtoType/ProtoType.py
+++ ProtoType/ProtoType.py
@@ -1,44 +1,33 @@
-#coding: utf8
+# coding: utf8
 
 import copy
-import unittest
 
 """
-copy.copy是浅拷贝，
-    浅拷贝只拷贝对象本身，而不拷贝对象引用的其他对象
-copy.deepcopy是深拷贝，
-    深拷贝不仅会拷贝对象本身，还会**递归地**拷贝对象中引用的其他对象
+copy.copy() 提供浅拷贝功能，
+    浅拷贝只拷贝对象本身，不拷贝对象引用的其它对象
+
+copy.deepcopy() 提供深拷贝功能，
+    深拷贝不仅会拷贝对象本身，还会递归地拷贝对象中引用的其它对象
 """
 
-class Test:
-    def __init__(self):
-        self.list = [1, 2, 3]
-        self.str = "str"
 
-class CopyTest(unittest.TestCase):
-    def test_shallow_copy(self):
-        test = Test()
-        test_shallow_copy = copy.copy(test)
+class ProtoType(object):
+    def copy(self):
+        return copy.copy(self)
 
-        test.list.pop()
-        self.assertEqual(test_shallow_copy.list, [1, 2])
+    def deep_copy(self):
+        return copy.deepcopy(self)
 
-    def test_immutable_types(self):
-        test = Test()
-        test_shallow_copy = copy.copy(test)
-
-        test.str = "str2"
-        self.assertNotEqual(test_shallow_copy.str, "str2")
-        self.assertEqual(test_shallow_copy.str, "str")
-
-    def test_deep_copy(self):
-        test = Test()
-        test_deep_copy = copy.deepcopy(test)
-
-        test.list.pop()
-        self.assertNotEqual(test.list, test_deep_copy.list)
-        self.assertEqual(test_deep_copy.list, [1, 2, 3])
 
 if __name__ == "__main__":
-    unittest.main()
+    class Test(ProtoType):
+        def __init__(self):
+            self.obj = object()
 
+    test = Test()
+
+    assert test.copy() is not test.copy()
+    assert test.copy().obj is test.copy().obj
+
+    assert test.deep_copy() is not test.deep_copy()
+    assert test.deep_copy().obj is not test.deep_copy().obj
